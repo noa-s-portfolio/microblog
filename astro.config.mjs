08bda9bf89eeb7b2e0db962e5a26fb6e27182101
y-tomimoto <geektomimoto0104@gmail.com>
2025-01-14 23:24:33 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://lexingtonthemes.com',
+  site: 'https://noa-s-portfolio.github.io',
   integrations: [sitemap(), mdx()]
 });
